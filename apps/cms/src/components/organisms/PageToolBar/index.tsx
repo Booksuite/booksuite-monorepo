322d50ee55c7015a935fc088efd08a1aabec1ddf
KevinGirelli <kevingirelli.gg@gmail.com>
2025-02-17 14:49:36 -0300

    Added PageToolBar, Error and Loading Components

---
+++ apps/cms/src/components/organisms/PageToolBar/index.tsx
@@ -0,0 +1,9 @@
+import { PageToolbarBackLink } from './PageToolbarBackLink'
+import { PageToolbarRoot } from './PageToolbarRoot'
+import { PageToolbarTitle } from './PageToolbarTitle'
+
+export const PageToolbar = {
+    Root: PageToolbarRoot,
+    BackLink: PageToolbarBackLink,
+    Title: PageToolbarTitle,
+}
