35fe118d7fc5432d28633752316a58ce14dc461c
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-02-20 09:15:41 -0300

    Added Dashboard Login (#8)
    * Added Dashboard Login
    
    * DashboadLoginForm Fixes

--- apps/cms/src/app/(public)/login/page.tsx
+++ apps/cms/src/app/(public)/login/page.tsx
@@ -1,9 +1,9 @@
+import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
-import { getServerSession } from 'next-auth'
 
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'
-import LoginForm from '@/components/login/Form'
+import DashboadLoginForm from '@/components/templates/DashboadLoginForm'
 
 export default async function Login() {
     const session = await getServerSession(authOptions)
@@ -14,11 +14,11 @@ export default async function Login() {
 
     return (
         <div className="Login">
-            <LoginForm />
+            <DashboadLoginForm />
 
             <Link
                 className="text-center forgot-pass"
-                href={'/esqueci-minha-senha'}
+                href={'/forgot-my-password'}
             >
                 Esqueceu a senha?
             </Link>
