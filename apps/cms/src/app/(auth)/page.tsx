0a1ad2751710abdf4b74125629661e76cfe0391e
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-02-26 10:59:02 -0300

    Remove Scss from Auth Components Layout
    * Remove Scss from Auth Components Layout
    
    * Fixes In Auth Layout

--- apps/cms/src/app/(auth)/page.tsx
+++ apps/cms/src/app/(auth)/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@chakra-ui/react'
-import Link from 'next/link'
 import { getServerSession } from 'next-auth'
+import Link from 'next/link'
 
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'
 
@@ -8,7 +8,7 @@ export default async function Home() {
     const session = await getServerSession(authOptions)
 
     return (
-        <main className="">
+        <main>
             <h1>Olá, {session?.user?.name}</h1>
 
             <Button as={Link} href={'/nova-empresa'}>
