5afb9f497fa102e6d1bcbd696d3287025ac4a4ff
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-02-26 10:18:00 -0300

    Removing Scss From Public Pages

--- apps/cms/src/app/(public)/login/page.tsx
+++ apps/cms/src/app/(public)/login/page.tsx
@@ -1,9 +1,10 @@
 import { getServerSession } from 'next-auth'
-import Link from 'next/link'
+import NextLink from 'next/link'
 import { redirect } from 'next/navigation'
 
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'
 import DashboadLoginForm from '@/components/templates/DashboadLoginForm'
+import { Link } from '@chakra-ui/react'
 
 export default async function Login() {
     const session = await getServerSession(authOptions)
@@ -17,8 +18,12 @@ export default async function Login() {
             <DashboadLoginForm />
 
             <Link
-                className="text-center forgot-pass"
-                href={'/forgot-my-password'}
+                as={NextLink}
+                href="/forgot-my-password"
+                display="block"
+                color="blue.500"
+                fontWeight="regular"
+                fontSize={18}
             >
                 Esqueceu a senha?
             </Link>
