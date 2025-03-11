46defe37c548b2359e8ae67f12a54f7b19f49a7a
KevinGirelli <kevingirelli.gg@gmail.com>
2025-03-11 09:26:28 -0300

    Enhance UI components with improved styling and color adjustments

--- apps/cms/src/components/organisms/PageHeader/PageHeaderTitle.tsx
+++ apps/cms/src/components/organisms/PageHeader/PageHeaderTitle.tsx
@@ -1,6 +1,11 @@
 import { Heading } from '@chakra-ui/react'
+
 import { PageHeaderTitleProps } from './types'
 
 export const PageHeaderTitle: React.FC<PageHeaderTitleProps> = (props) => {
-    return <Heading as="h1">{props.children}</Heading>
+    return (
+        <Heading as="h1" fontSize={'1.25rem'}>
+            {props.children}
+        </Heading>
+    )
 }
