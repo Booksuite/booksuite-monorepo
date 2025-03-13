9436be8f9218f547a39a979e2d568209c6cb570f
KevinGirelli <kevingirelli.gg@gmail.com>
2025-03-13 16:36:21 -0300

    Fixes In InputBox Component

--- apps/cms/src/components/atoms/InputBox/index.tsx
+++ apps/cms/src/components/atoms/InputBox/index.tsx
@@ -2,6 +2,7 @@
 
 import { FormControl, FormLabel, Input } from '@chakra-ui/react'
 import { ElementType, useState } from 'react'
+import CurrencyInput from 'react-currency-input-field'
 import InputMask from 'react-input-mask'
 
 import { InputBoxProps } from './types'
@@ -16,30 +17,55 @@ function InputBox({ asText = false, onValueChange, ...props }: InputBoxProps) {
             {...props.formControl}
             className={`InputBox ${props.formControl?.className}`}
         >
-            {asText === false && (
-                <input
-                    type="hidden"
-                    name={props.name}
-                    value={inputHiddenValue}
-                />
+            {props.type === 'currency' ? (
+                <>
+                    {asText === false && (
+                        <input
+                            type="hidden"
+                            name={props.name}
+                            value={inputHiddenValue}
+                        />
+                    )}
+                    <Input
+                        as={CurrencyInput}
+                        prefix={props.prefix ?? 'R$ '}
+                        placeholder=" "
+                        decimalsLimit={2}
+                        decimalScale={2}
+                        onValueChange={(value, name, values) => {
+                            setInputHiddenValue(values.float)
+                            onValueChange(value, name, values)
+                        }}
+                        {...props}
+                        name={asText ? props.name : ''}
+                    />
+                </>
+            ) : (
+                <>
+                    {asText === false && (
+                        <input
+                            type="hidden"
+                            name={props.name}
+                            value={inputHiddenValue}
+                        />
+                    )}
+                    <Input
+                        as={
+                            props?.mask
+                                ? (InputMask as unknown as ElementType)
+                                : undefined
+                        }
+                        value={inputHiddenValue}
+                        onChange={(e) => {
+                            const value = e.target.value
+                            setInputHiddenValue(value)
+                            onValueChange(value, props.name, {})
+                        }}
+                        {...props}
+                        name={asText ? props.name : ''}
+                    />
+                </>
             )}
-
-            <Input
-                as={
-                    props?.mask
-                        ? (InputMask as unknown as ElementType)
-                        : undefined
-                }
-                value={inputHiddenValue}
-                onChange={(e) => {
-                    const value = e.target.value
-                    setInputHiddenValue(value)
-                    onValueChange(value, props.name, {})
-                }}
-                {...props}
-                name={asText ? props.name : ''}
-            />
-
             <FormLabel>{props.label}</FormLabel>
         </FormControl>
     )
