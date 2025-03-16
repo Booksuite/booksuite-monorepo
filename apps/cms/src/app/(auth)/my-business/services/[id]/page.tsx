562bc9c83a417f03ff8ef04998a686bd4d622c36
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-03-16 17:34:51 -0300

    Added Service Edit

--- apps/cms/src/app/(auth)/my-business/services/[id]/page.tsx
+++ apps/cms/src/app/(auth)/my-business/services/[id]/page.tsx
@@ -1,91 +1,66 @@
 'use client'
 
-import { useGetServiceById, useUpdateService } from '@booksuite/sdk'
-import { Flex, Spinner, useToast } from '@chakra-ui/react'
+import { updateService, useGetServiceById } from '@booksuite/sdk'
+import { useToast } from '@chakra-ui/react'
 import { Formik } from 'formik'
-import { type FormEvent, useState } from 'react'
 
+import { ServiceForm } from '@/app/(auth)/my-business/services/serviceForm'
 import { useCurrentCompanyId } from '@/common/contexts/user'
-import { updateExtra } from '@/common/services/extra/updateExtra'
-import { UpdateExtraDTO } from '@/common/types/Extra'
 import { getErrorMessage } from '@/common/utils'
-import { SwitchBox } from '@/components/atoms/SwitchBox'
-import { toastGenericPatchMessages } from '@/components/molecules/ToastMessages'
 import { PageHeader } from '@/components/organisms/PageHeader'
-import { DashboardExperienceForm } from '@/components/templates/DashboardExperienceForm'
-import { DashboardExtraForm } from '@/components/templates/DashboardExtraForm'
-import { createFormInitialValues, ServiceFormData, serviceFormSchema } from '../utils/config'
+import {
+    createFormInitialValues,
+    ServiceFormData,
+    serviceFormSchema,
+} from '../utils/config'
 
-export default function UpdateService({
-    params,
-}: {
+interface UpdateServiceProps {
     params: { id: string }
-}) {
+}
+
+export default function UpdateService({ params }: UpdateServiceProps) {
     const companyId = useCurrentCompanyId()
 
-    const {data: service} = useGetServiceById({
+    const { data: service } = useGetServiceById({
         companyId,
-        id: params.id
+        id: params.id,
     })
-
-    const {mutateAsync: updateService} = useUpdateService()
 
     const toast = useToast()
 
-    async function handleSubmit(
-        formData: ServiceFormData
-    ) {
-    try{
-        await updateService({
-                    companyId,
-                    data: formData,
-                    id: params.id
-                })
-    
-                toast({
-                    title: 'Experiência modificada com sucesso',
-                    status: 'success'
-                })
-            }catch(error){
-                toast({
-                    title: 'Erro ao modificar experiência',
-                    description: getErrorMessage(error),
-                    status: 'error',
-                })
-            }
+    async function handleSubmit(formData: ServiceFormData) {
+        try {
+            await updateService({ id: params.id, companyId }, formData)
+
+            toast({
+                title: 'Experiência editada com sucesso',
+                status: 'success',
+            })
+        } catch (error) {
+            toast({
+                title: 'Erro ao editar experiência',
+                description: getErrorMessage(error),
+                status: 'error',
+            })
+        }
     }
 
     return (
         <div>
-            <PageHeader.Root>
-                <Flex
-                    alignItems="center"
-                    justifyContent="space-between"
-                    gap={2}
+            <PageHeader
+                title="Editar Experiências"
+                backButtonHref="/my-business/services"
+                backLButtonLabel="Meu Negócio"
+            />
+
+            {!!service && (
+                <Formik<ServiceFormData>
+                    initialValues={createFormInitialValues(service)}
+                    validationSchema={serviceFormSchema}
+                    onSubmit={handleSubmit}
                 >
-                    <PageHeader.BackLink href="/my-business/extras">
-                        Extras
-                    </PageHeader.BackLink>
-
-                    <SwitchBox
-                        label="Ativa"
-                        id="status"
-                        name="status"
-                        defaultChecked
-                    />
-                </Flex>
-
-                <PageHeader.Title>Detalhes do Extra</PageHeader.Title>
-            </PageHeader.Root>
-
-            {!!service && 
-            ( <Formik<ServiceFormData>
-                            initialValues={createFormInitialValues(service)}
-                            validationSchema={serviceFormSchema}
-                            onSubmit={handleSubmit}
-                        >
-                        <DashboardExperienceForm />
-                        </Formik>
+                    <ServiceForm />
+                </Formik>
             )}
         </div>
     )
