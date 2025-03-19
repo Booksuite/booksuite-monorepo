4431819c484db6ddae7d855f9e4a79d2aebaa28f
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-03-19 12:20:42 -0300

    Settings Page
    * Settings Page
    
    * Changes In Icons
    
    * Changes In Amenities Icon

--- apps/cms/src/app/(auth)/configuracoes/layout.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-interface LayoutConfiguracoesProps {
-    children: React.ReactNode
-}
-
-export default function LayoutConfiguracoes(props: LayoutConfiguracoesProps) {
-    return <>{props.children}</>
-}
