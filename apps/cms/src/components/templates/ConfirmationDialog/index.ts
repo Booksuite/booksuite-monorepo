6cd7e05002ae533e76ebe97a55ac9c54c739f559
Daniel K Guolo <daniel_kv@hotmail.com>
2025-03-19 20:44:45 -0300

    add dialog and menu

---
+++ apps/cms/src/components/templates/ConfirmationDialog/index.ts
@@ -0,0 +1,7 @@
+export { ConfirmationDialog } from './component'
+export { closeDialog, openDialog } from './helpers'
+export { useConfirmationDialog } from './hooks'
+export type {
+    DialogButton as ConfirmationDialogButton,
+    ConfirmationDialogVariant,
+} from './store'
