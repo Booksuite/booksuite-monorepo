85ede1e770932b3f22ba11c3e0256a122c4d13e5
KevinGirelli <kevingirelli.gg@gmail.com>
2025-03-20 09:29:30 -0300

    Created config.ts

---
+++ apps/cms/src/app/(auth)/settings/address/utils/config.ts
@@ -0,0 +1,35 @@
+import { Company } from '@booksuite/sdk'
+import * as yup from 'yup'
+
+export type AddressFormData = Pick<
+    Company,
+    | 'stateRegistration'
+    | 'municipalRegistration'
+    | 'address'
+    | 'number'
+    | 'country'
+    | 'state'
+    | 'city'
+>
+
+export const createAddressInitialValues = (
+    data?: Partial<Company>,
+): AddressFormData => ({
+    stateRegistration: data?.stateRegistration || null,
+    municipalRegistration: data?.municipalRegistration || null,
+    address: data?.address || '',
+    number: data?.number || '',
+    country: data?.country || '',
+    state: data?.state || '',
+    city: data?.city || '',
+})
+
+export const addressSchema = yup.object({
+    stateRegistration: yup.string().nullable(),
+    municipalRegistration: yup.string().nullable(),
+    address: yup.string().required('O endereço é obrigatório'),
+    number: yup.string().required('O número é obrigatório'),
+    country: yup.string().required('O país é obrigatório'),
+    state: yup.string().required('O estado é obrigatório'),
+    city: yup.string().required('A cidade é obrigatória'),
+})
