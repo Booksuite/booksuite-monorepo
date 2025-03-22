46bdb9b7f31f3bada5cd4172b93dffab877a5df0
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-03-22 20:32:53 -0300

    Cancellation Policy Description

--- apps/cms/src/app/(auth)/settings/cancellation-policy/components/CancellationPolicyForm.tsx
+++ apps/cms/src/app/(auth)/settings/cancellation-policy/components/CancellationPolicyForm.tsx
@@ -1,6 +1,7 @@
 import {
     Box,
     Button,
+    Flex,
     HStack,
     IconButton,
     Stack,
@@ -9,7 +10,7 @@ import {
     VStack,
 } from '@chakra-ui/react'
 import { FieldArray, Form, useFormikContext } from 'formik'
-import { CircleMinus, CirclePlus } from 'lucide-react'
+import { CircleMinus, CirclePlus, ScanText } from 'lucide-react'
 
 import InputBox from '@/components/atoms/InputBox'
 import { InputNumberBox } from '@/components/atoms/InputNumberBox'
@@ -226,6 +227,111 @@ export const CancellationPolicyForm = () => {
                         )}
                     </FieldArray>
                 )}
+
+                <VStack alignItems={'start'}>
+                    <h2 style={{ fontWeight: '600' }}>
+                        Descrição da política de cancelamento
+                    </h2>
+                    <Box
+                        bg={'gray.100'}
+                        p={3}
+                        borderRadius={'md'}
+                        display={'flex'}
+                        alignItems={'center'}
+                    >
+                        {/*Text Exemple*/}
+                        <Text
+                            fontSize={'md'}
+                            color={'#0B1F51'}
+                            p={3}
+                            border={0}
+                        >
+                            <h4>Descrição dinâmica (exibida para o hóspede)</h4>
+                            1. Cancelamento ou troca após o período de
+                            desistência obrigatório de 7 dias após a data de
+                            efetivação da reserva será cobrado uma taxa de
+                            cancelamento de 20% sobre o valor total da reserva;
+                            <br />
+                            <br />
+                            2. O período de desistência obrigatório só é válido,
+                            caso a reserva seja efetuada com antecedência mínima
+                            de 8 dias até o check-in. Caso contrário, será
+                            aplicado a taxa de cancelamento padrão;
+                            <br />
+                            <br />
+                            3. Cancelamento ou troca em até 14 dias antes do
+                            check-in, será cobrado uma multa de 50% do valor
+                            total da reserva;
+                            <br />
+                            <br />
+                            4. Cancelamento ou troca em até 4 dias antes do
+                            check-in, será cobrado uma multa de 100% do valor
+                            total da reserva;
+                        </Text>
+                    </Box>
+
+                    {/*Text Exemple*/}
+                    <Text
+                        fontSize={'md'}
+                        color={'#0B1F51'}
+                        p={3}
+                        border={'1px solid #D9E2EC'}
+                        borderRadius={'md'}
+                    >
+                        <h4>Demais regras e observações</h4>
+                        - Em caso de saída antecipada durante a estadia, não
+                        haverá reembolso do valor pago e/ou será efetuada a
+                        cobrança do valor em aberto referente ao total da
+                        reserva.
+                        <br />
+                        <br />
+                        - Em caso de alteração de datas da estadia serão
+                        aplicadas as políticas e taxas de cancelamento. Caso a
+                        nova data possua uma tarifa diferente da primeira data,
+                        será acertado a diferença além da multa padrão, seja
+                        para mais ou para menos.
+                        <br />
+                        <br />- Em casos extremos de fechamento das atividades
+                        por ordem de governos, autoridades locais de saúde ou
+                        qualquer outro motivo de caráter excepcional, todas
+                        reservas no período serão reagendadas, sem custo
+                        adicional, desde que o tarifário da primeira data
+                        reserva mantenha o mesmo valor da nova data, caso
+                        contrário, será acertado a diferença. Demais condições
+                        não previstas neste descritivo serão esclarecidas caso a
+                        caso, conforme regras e políticas internas.
+                    </Text>
+
+                    <Flex
+                        bg={'gray.100'}
+                        p={3}
+                        borderRadius={'md'}
+                        flexDirection="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        width={'100%'}
+                        textAlign={'center'}
+                        gap={4}
+                    >
+                        <br />
+                        <br />
+                        <ScanText size={'40'} />
+                        <h3>
+                            Se necessário personalize nosso modelo de políticas
+                            de cancelamento para o seu negócio.
+                        </h3>
+                        <Button
+                            variant="outline"
+                            width={'100%'}
+                            leftIcon={<CirclePlus size={23} />}
+                            mb={4}
+                            border={0}
+                            fontSize={'md'}
+                        >
+                            Selecionar Modelo
+                        </Button>
+                    </Flex>
+                </VStack>
 
                 <Button type="submit">Salvar</Button>
             </Stack>
