371f7f4f32815d5f1da70097a4c74360200f8d5f
KevinGirelli <kevingirelli.gg@gmail.com>
2025-03-21 23:39:50 -0300

    Created Custom Penalty Rates

--- apps/cms/src/app/(auth)/settings/cancellation-policy/components/CancellationPolicyForm.tsx
+++ apps/cms/src/app/(auth)/settings/cancellation-policy/components/CancellationPolicyForm.tsx
@@ -2,12 +2,14 @@ import {
     Box,
     Button,
     HStack,
+    IconButton,
     Stack,
     Switch,
     Text,
     VStack,
 } from '@chakra-ui/react'
-import { Form, useFormikContext } from 'formik'
+import { FieldArray, Form, useFormikContext } from 'formik'
+import { CircleMinus, CirclePlus } from 'lucide-react'
 
 import InputBox from '@/components/atoms/InputBox'
 import { InputNumberBox } from '@/components/atoms/InputNumberBox'
@@ -106,6 +108,125 @@ export const CancellationPolicyForm = () => {
                         </Box>
                     </VStack>
                 )}
+
+                <HStack justifyContent={'space-between'} mt={4}>
+                    <h2 style={{ fontWeight: '600' }}>
+                        Aplicar taxas de penalização personalizadas
+                    </h2>
+                    <Switch
+                    /*isChecked={values.enablePenaltyRanges}
+                        onChange={(e) =>
+                            setFieldValue(
+                                'enablePenaltyRanges',
+                                e.target.checked,
+                            )
+                        }*/
+                    />
+                </HStack>
+
+                {/* add {values.enablePenaltyRanges && (*/}
+                {values.defaultPenaltyBy && (
+                    <FieldArray name="penaltyRanges">
+                        {({ push, remove }) => (
+                            <VStack alignItems={'start'}>
+                                {values.penaltyRanges.map((penalty, index) => (
+                                    <Stack
+                                        key={index}
+                                        p={4}
+                                        w="100%"
+                                        spacing={4}
+                                    >
+                                        <HStack
+                                            justifyContent={'space-between'}
+                                        >
+                                            <h3>
+                                                Taxa de penalização {index + 1}
+                                            </h3>
+                                            <IconButton
+                                                icon={<CircleMinus />}
+                                                colorScheme="red"
+                                                variant="ghost"
+                                                onClick={() => remove(index)}
+                                                aria-label={''}
+                                            />
+                                        </HStack>
+                                        <InputNumberBox
+                                            label="Dias antes do check-in"
+                                            {...getFieldProps(
+                                                `penaltyRanges.${index}.daysBeforeCheckIn`,
+                                            )}
+                                            onChange={handleChange(
+                                                `penaltyRanges.${index}.daysBeforeCheckIn`,
+                                            )}
+                                        />
+                                        <SelectBox
+                                            label="Seletor de penalização"
+                                            {...getFieldProps(
+                                                `penaltyRanges.${index}.penaltyBy`,
+                                            )}
+                                        />
+                                        <InputBox
+                                            label="Valor em %"
+                                            {...getFieldProps(
+                                                `penaltyRanges.${index}.value`,
+                                            )}
+                                        />
+                                        <Button
+                                            mt={3}
+                                            variant="outline"
+                                            width={'100%'}
+                                            leftIcon={<CirclePlus size={16} />}
+                                            mb={4}
+                                            onClick={() =>
+                                                push({
+                                                    daysBeforeCheckIn: '',
+                                                    penaltyBy:
+                                                        'RESERVATION_PERCENTAGE',
+                                                    value: '',
+                                                })
+                                            }
+                                        >
+                                            Adicionar taxa de penalização
+                                        </Button>
+                                        <Box
+                                            bg={'gray.100'}
+                                            p={3}
+                                            borderRadius={'md'}
+                                            display={'flex'}
+                                            alignItems={'center'}
+                                        >
+                                            <Text
+                                                fontSize={'md'}
+                                                color={'#0B1F51'}
+                                            >
+                                                As taxas adicionais de
+                                                cancelamento são multas
+                                                aplicadas para cancelamentos
+                                                próximos à data do check-in,
+                                                podendo haver uma ou mais taxas
+                                                adicionais.
+                                                <br />
+                                                <br />
+                                                <b>Atenção:</b> esta taxa não é
+                                                cumulativa com a taxa de
+                                                cancelamento padrão.
+                                                <br />
+                                                <br />
+                                                Essa cobrança visa compensar
+                                                possíveis perdas do
+                                                estabelecimento, garantindo um
+                                                ressarcimento adequado diante do
+                                                risco de não conseguir realocar
+                                                a acomodação no mesmo período.
+                                            </Text>
+                                        </Box>
+                                    </Stack>
+                                ))}
+                            </VStack>
+                        )}
+                    </FieldArray>
+                )}
+
                 <Button type="submit">Salvar</Button>
             </Stack>
         </Form>
