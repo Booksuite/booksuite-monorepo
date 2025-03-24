e740a88ce2b45c9a6ae3b126c2fe648570fb4035
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-03-24 13:34:00 -0300

    AddressForm Visual Refactoring

--- apps/cms/src/app/(auth)/settings/address/components/AddressForm.tsx
+++ apps/cms/src/app/(auth)/settings/address/components/AddressForm.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { Button, Stack, useToast } from '@chakra-ui/react'
+import { Button, Grid, GridItem, useToast } from '@chakra-ui/react'
 import cep from 'cep-promise'
 import { Form, useFormikContext } from 'formik'
 import { debounce } from 'radash'
 import { useEffect, useRef, useState } from 'react'
 
 import InputBox from '@/components/atoms/InputBox'
-import { AddressFormData } from '../utils/config'
+import type { AddressFormData } from '../utils/config'
 
 export const AddressForm = () => {
     const [searchInputValue, setSearchInputValue] = useState<string>('')
@@ -41,83 +41,112 @@ export const AddressForm = () => {
 
     return (
         <Form>
-            <Stack spacing={4}>
-                <InputBox
-                    label="CEP"
-                    {...getFieldProps('zipcode')}
-                    onChange={(e) => {
-                        setSearchInputValue(e.target.value)
-                        getFieldProps('zipcode').onChange(e)
-                    }}
-                />
-                <InputBox
-                    label="Endereço"
-                    error={errors.address}
-                    formControl={{
-                        isInvalid: !!errors.address && touched.address,
-                    }}
-                    {...getFieldProps('address')}
-                />
-                <InputBox
-                    label="Número"
-                    error={errors.number}
-                    formControl={{
-                        isInvalid: !!errors.number && touched.number,
-                    }}
-                    {...getFieldProps('number')}
-                />
-                <InputBox
-                    label="País"
-                    error={errors.country}
-                    formControl={{
-                        isInvalid: !!errors.country && touched.country,
-                    }}
-                    {...getFieldProps('country')}
-                />
-                <InputBox
-                    label="Estado"
-                    error={errors.state}
-                    formControl={{ isInvalid: !!errors.state && touched.state }}
-                    {...getFieldProps('state')}
-                />
-                <InputBox
-                    label="Cidade"
-                    error={errors.city}
-                    formControl={{ isInvalid: !!errors.city && touched.city }}
-                    {...getFieldProps('city')}
-                />
-                <InputBox
-                    label="Inscrição Estadual"
-                    error={errors.stateRegistration}
-                    formControl={{
-                        isInvalid:
-                            !!errors.stateRegistration &&
-                            touched.stateRegistration,
-                    }}
-                    {...getFieldProps('stateRegistration')}
-                />
-                <InputBox
-                    label="Inscrição Municipal"
-                    error={errors.municipalRegistration}
-                    formControl={{
-                        isInvalid:
-                            !!errors.municipalRegistration &&
-                            touched.municipalRegistration,
-                    }}
-                    {...getFieldProps('municipalRegistration')}
-                />
-                <InputBox
-                    label="URL no Google Maps"
-                    /*error={errors.}
-                    formControl={{
-                        isInvalid: !!errors. && touched.,
-                    }}
-                    {...getFieldProps('')}*/
-                />
-                <Button type="submit" size="lg">
-                    Salvar
-                </Button>
-            </Stack>
+            <Grid templateColumns="repeat(12, 1fr)" gap={4}>
+                <GridItem colSpan={12}>
+                    <InputBox
+                        label="CEP"
+                        {...getFieldProps('zipcode')}
+                        onChange={(e) => {
+                            setSearchInputValue(e.target.value)
+                            getFieldProps('zipcode').onChange(e)
+                        }}
+                    />
+                </GridItem>
+
+                <GridItem colSpan={{ base: 12, md: 4 }}>
+                    <InputBox
+                        label="Cidade"
+                        error={errors.city}
+                        formControl={{
+                            isInvalid: !!errors.city && touched.city,
+                        }}
+                        {...getFieldProps('city')}
+                    />
+                </GridItem>
+                <GridItem colSpan={{ base: 12, md: 4 }}>
+                    <InputBox
+                        label="Estado"
+                        error={errors.state}
+                        formControl={{
+                            isInvalid: !!errors.state && touched.state,
+                        }}
+                        {...getFieldProps('state')}
+                    />
+                </GridItem>
+                <GridItem colSpan={{ base: 12, md: 4 }}>
+                    <InputBox
+                        label="País"
+                        error={errors.country}
+                        formControl={{
+                            isInvalid: !!errors.country && touched.country,
+                        }}
+                        {...getFieldProps('country')}
+                    />
+                </GridItem>
+
+                <GridItem colSpan={{ base: 12, md: 6 }}>
+                    <InputBox
+                        label="Endereço"
+                        error={errors.address}
+                        formControl={{
+                            isInvalid: !!errors.address && touched.address,
+                        }}
+                        {...getFieldProps('address')}
+                    />
+                </GridItem>
+                <GridItem colSpan={{ base: 12, md: 6 }}>
+                    <InputBox
+                        label="Número"
+                        error={errors.number}
+                        formControl={{
+                            isInvalid: !!errors.number && touched.number,
+                        }}
+                        {...getFieldProps('number')}
+                    />
+                </GridItem>
+
+                <GridItem colSpan={12}>
+                    <InputBox
+                        label="URL no Google Maps"
+                        /*error={errors.googleMapsUrl}
+                        formControl={{
+                            isInvalid: !!errors.googleMapsUrl && touched.googleMapsUrl,
+                        }}
+                        {...getFieldProps('googleMapsUrl')}*/
+                    />
+                </GridItem>
+
+                {/*<GridItem colSpan={{ base: 12, md: 6 }}>
+                    <InputBox
+                        label="Inscrição Estadual"
+                        error={errors.stateRegistration}
+                        formControl={{
+                            isInvalid:
+                                !!errors.stateRegistration &&
+                                touched.stateRegistration,
+                        }}
+                        {...getFieldProps('stateRegistration')}
+                    />
+                </GridItem>
+                <GridItem colSpan={{ base: 12, md: 6 }}>
+                    <InputBox
+                        label="Inscrição Municipal"
+                        error={errors.municipalRegistration}
+                        formControl={{
+                            isInvalid:
+                                !!errors.municipalRegistration &&
+                                touched.municipalRegistration,
+                        }}
+                        {...getFieldProps('municipalRegistration')}
+                    />
+                </GridItem>*/}
+
+                <GridItem colSpan={12}>
+                    <Button type="submit" size="lg" width="100%">
+                        Salvar
+                    </Button>
+                </GridItem>
+            </Grid>
         </Form>
     )
 }
