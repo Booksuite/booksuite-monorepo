10b6ee11012e6b0f02afe9c0e8765a9164e3c267
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-03-25 13:16:49 -0300

    Created Age Policy (#43)
    * Created Age Policy
    
    * age-policy Moved To Settings Folder
    
    * Changes In InputNumber On Age-Policy
    
    * Changes In Age-Policy Layout
    
    * Created Age Policy
    
    * age-policy Moved To Settings Folder
    
    * Changes In InputNumber On Age-Policy
    
    * Changes In Age-Policy Layout
    
    * half finished agePolicy
    
    * fixes
    
    * finished agePolicy
    
    * AgePolicyForm Visual Refactoring
    
    * Changes In AgePolicy
    
    * Created Age Policy
    
    * age-policy Moved To Settings Folder
    
    * Changes In InputNumber On Age-Policy
    
    * Changes In Age-Policy Layout
    
    * half finished agePolicy
    
    * Created Age Policy
    
    * age-policy Moved To Settings Folder
    
    * Changes In InputNumber On Age-Policy
    
    * Changes In Age-Policy Layout
    
    * fixes
    
    * finished agePolicy
    
    * AgePolicyForm Visual Refactoring
    
    * request changes
    
    ---------
    
    Co-authored-by: Zayma1 <zaymakinsiona@gmail.com>

---
+++ apps/cms/src/app/(auth)/settings/age-policy/utils/config.ts
@@ -0,0 +1,43 @@
+import { AgePolicyFull, AgePolicyInput } from '@booksuite/sdk'
+import * as yup from 'yup'
+
+export type AgePolicyFormData = AgePolicyInput
+
+export const createAgePolicyInitialValues = (
+    data?: Partial<AgePolicyFull>, 
+): AgePolicyFormData => ({
+    acceptChildren: data?.acceptChildren || false,
+    adultMinAge: data?.adultMinAge || 0,
+    ageGroups:
+        data?.ageGroups.map((group) => ({
+            id: group.id,
+            initialAge: group.initialAge,
+            finalAge: group.finalAge,
+            chargeType: group.chargeType,
+            value: group.value || 0
+        })) || [],
+})
+
+export const agePolicyFormSchema = yup.object({
+    acceptChildren: yup.boolean().required('Campo obrigatório'),
+    adultMinAge: yup
+        .number()
+        .required('Idade mínima para adultos é obrigatória'),
+    ageGroups: yup.array().of(
+        yup.object({
+            initialAge: yup
+                .number()
+                .required('Idade mínima do grupo é obrigatória')
+                .min(0, 'A idade mínima deve ser pelo menos 0'),
+            finalAge: yup
+                .number()
+                .required('Idade máxima do grupo é obrigatória')
+                .min(
+                    yup.ref('initialAge'),
+                    'A idade máxima deve ser maior que a mínima',
+                ),
+            chargeType: yup.string().required('Tipo de cobrança é obrigatório'),
+            value: yup.number().required('Preço de cobrança obrigátorio'),
+        }),
+    ),
+})
---
+++ apps/cms/src/app/(auth)/settings/age-policy/utils/constants.ts
@@ -0,0 +1,7 @@
+import { AgeGroupDTOChargeType } from "@booksuite/sdk";
+
+export const AGE_GROUP_CHARGE_TYPE: Record<AgeGroupDTOChargeType, String> = {
+    DAILY_PER_CHILDREN: 'Diária por Criança',
+    FREE: 'Grátis',
+    DAILY_PERCENTAGE_PER_CHILDREN: 'Percentual diário por criança'
+}
