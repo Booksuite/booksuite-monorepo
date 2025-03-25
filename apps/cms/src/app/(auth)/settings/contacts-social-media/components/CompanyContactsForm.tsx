afbf9b9fa5205732dddb337c578f75ef54f36d4c
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-03-25 15:58:02 -0300

    Changes In Company Contacts Form

--- apps/cms/src/app/(auth)/settings/contacts-social-media/components/CompanyContactsForm.tsx
+++ apps/cms/src/app/(auth)/settings/contacts-social-media/components/CompanyContactsForm.tsx
@@ -1,68 +1,172 @@
-import { IconButton, Stack, useToast, VStack } from '@chakra-ui/react'
+import { Button, HStack, IconButton, Stack } from '@chakra-ui/react'
 import { FieldArray, useFormikContext } from 'formik'
-import { CirclePlus } from 'lucide-react'
+import { CirclePlus, Trash } from 'lucide-react'
 
 import InputBox from '@/components/atoms/InputBox'
 import { ContactsData } from '../utils/config'
 
 export default function CompanyContactsForm() {
-    const toast = useToast()
-
-    const {
-        getFieldProps,
-        touched,
-        errors,
-        values,
-        handleChange,
-        setFieldValue,
-    } = useFormikContext<ContactsData>()
+    const { getFieldProps, touched, errors, values, handleChange } =
+        useFormikContext<ContactsData>()
 
     return (
         <div className="CompanyContactForm">
-            <FieldArray name="emails">
+            <FieldArray name="contacts">
                 {({ push, remove }) => (
                     <Stack spacing={4}>
-                        {values.contacts && values.contacts.length > 0 ? (
-                            values.contacts.map((_, index) => (
-                                <VStack
-                                    key={index}
-                                    spacing={2}
-                                    alignItems="start"
-                                >
-                                    <InputBox
-                                        label="Email"
-                                        type="text"
-                                        error={errors?.contacts}
-                                        formControl={{
-                                            isInvalid:
-                                                !!errors?.contacts &&
-                                                touched.contacts,
-                                        }}
-                                        {...getFieldProps(
-                                            `contacts.${index}.value`,
-                                        )}
-                                        onChange={handleChange(
-                                            `contacts.${index}.value`,
-                                        )}
-                                    />
-                                </VStack>
-                            ))
+                        <h3 style={{ fontWeight: '600', marginBottom: '0' }}>
+                            E-mails de Contato
+                        </h3>
+                        {values.contacts?.filter(
+                            (contact) => contact.type === 'email',
+                        ).length ? (
+                            values.contacts.map(
+                                (contact, index) =>
+                                    contact.type === 'email' && (
+                                        <HStack
+                                            key={index}
+                                            spacing={2}
+                                            alignItems="center"
+                                            width="100%"
+                                        >
+                                            <InputBox
+                                                label="E-mail de Reserva"
+                                                type="email"
+                                                /*error={
+                                                    errors?.contacts?.[index]
+                                                        ?.value
+                                                }
+                                                formControl={{
+                                                    isInvalid:
+                                                        !!errors?.contacts?.[
+                                                            index
+                                                        ] &&
+                                                        touched.contacts?.[
+                                                            index
+                                                        ],
+                                                }}
+                                                {...getFieldProps(
+                                                    `contacts.${index}.value`,
+                                                )}
+                                                onChange={handleChange(
+                                                    `contacts.${index}.value`,
+                                                )}*/
+                                            />
+                                            <IconButton
+                                                icon={<Trash />}
+                                                colorScheme="red"
+                                                variant="ghost"
+                                                onClick={() => remove(index)}
+                                                aria-label={'Remover'}
+                                                size={'lg'}
+                                            />
+                                        </HStack>
+                                    ),
+                            )
                         ) : (
                             <h2>Nenhum Email encontrado</h2>
                         )}
 
-                        <IconButton
-                            icon={<CirclePlus />}
-                            colorScheme="blue"
-                            variant="ghost"
-                            onClick={() =>
-                                push({
-                                    type: 'email',
-                                    value: '',
-                                })
-                            }
-                            aria-label="add"
-                        />
+                        <Button
+                            variant="outline"
+                            width={'100%'}
+                            leftIcon={<CirclePlus />}
+                            size={'lg'}
+                            onClick={() => push({ type: 'email', value: '' })}
+                        >
+                            Adicionar Email
+                        </Button>
+
+                        <h3 style={{ fontWeight: '600', marginTop: '20px' }}>
+                            Telefones (Opcional)
+                        </h3>
+                        {values.contacts?.filter(
+                            (contact) => contact.type === 'phone',
+                        ).length ? (
+                            values.contacts.map(
+                                (contact, index) =>
+                                    contact.type === 'phone' && (
+                                        <HStack
+                                            key={index}
+                                            spacing={2}
+                                            alignItems="center"
+                                            width="100%"
+                                        >
+                                            <InputBox
+                                                label="Telefone"
+                                                type="tel"
+                                                /*error={
+                                                    errors?.contacts?.[index]
+                                                        ?.value
+                                                }
+                                                formControl={{
+                                                    isInvalid:
+                                                        !!errors?.contacts?.[
+                                                            index
+                                                        ] &&
+                                                        touched.contacts?.[
+                                                            index
+                                                        ],
+                                                }}
+                                                {...getFieldProps(
+                                                    `contacts.${index}.value`,
+                                                )}*/
+                                                onChange={handleChange(
+                                                    `contacts.${index}.value`,
+                                                )}
+                                            />
+                                            <IconButton
+                                                icon={<Trash />}
+                                                colorScheme="red"
+                                                variant="ghost"
+                                                onClick={() => remove(index)}
+                                                aria-label={'Remover'}
+                                                size={'lg'}
+                                            />
+                                        </HStack>
+                                    ),
+                            )
+                        ) : (
+                            <h2>Nenhum telefone encontrado</h2>
+                        )}
+
+                        <Button
+                            variant="outline"
+                            width={'100%'}
+                            leftIcon={<CirclePlus />}
+                            size={'lg'}
+                            onClick={() => push({ type: 'phone', value: '' })}
+                        >
+                            Adicionar Telefone
+                        </Button>
+
+                        <h3 style={{ fontWeight: '600', marginTop: '20px' }}>
+                            Redes Sociais (Opcional)
+                        </h3>
+                        {['instagram', 'facebook', 'linkedin', 'x'].map(
+                            (social, index) => (
+                                <InputBox
+                                    key={social}
+                                    label={
+                                        social.charAt(0).toUpperCase() +
+                                        social.slice(1)
+                                    }
+                                    type="text"
+                                    /*error={errors?.contacts?.[index]?.value}
+                                    formControl={{
+                                        isInvalid:
+                                            !!errors?.contacts?.[index] &&
+                                            touched.contacts?.[index],
+                                    }}
+                                    {...getFieldProps(
+                                        `contacts.${index}.value`,
+                                    )}
+                                    onChange={handleChange(
+                                        `contacts.${index}.value`,
+                                    )}*/
+                                />
+                            ),
+                        )}
                     </Stack>
                 )}
             </FieldArray>
