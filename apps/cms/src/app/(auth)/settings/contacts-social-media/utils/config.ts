63ee973b73f5de6be65f68fc4fe9337bc7bffca4
Zayma1 <zaymakinsiona@gmail.com>
2025-03-26 09:03:03 -0300

    changes

--- apps/cms/src/app/(auth)/settings/contacts-social-media/utils/config.ts
+++ apps/cms/src/app/(auth)/settings/contacts-social-media/utils/config.ts
@@ -1,8 +1,4 @@
-import {
-    CompanyContactResponseDTOType,
-    CompanyCreateInput,
-    CompanyFull,
-} from '@booksuite/sdk'
+import { CompanyCreateInput, CompanyFull } from '@booksuite/sdk'
 import * as yup from 'yup'
 
 export type ContactsData = Pick<CompanyCreateInput, 'contacts'>
@@ -14,12 +10,5 @@ export const createContactsFormInitialValues = (
 })
 
 export const companyContactSchema = yup.object({
-    type: yup
-        .mixed<CompanyContactResponseDTOType>()
-        .oneOf(
-            ['phone', 'email', 'instagram', 'facebook', 'linkedin', 'x'],
-            'Tipo de contato inválido',
-        )
-        .required('O tipo de contato é obrigatório'),
-    value: yup.string().required('Contato é Obrigátorio'),
+    contacts: yup.array().min(1),
 })
