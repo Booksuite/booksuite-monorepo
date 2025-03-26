0d5d037ff1c4ce5b1246f09831e9549c032995b2
KevinGirelli <kevingirelli.gg@gmail.com>
2025-03-26 10:35:22 -0300

    Fixes In CheckboxGroup

---
+++ apps/cms/src/app/(auth)/settings/hosting-rules/utils/contants.ts
@@ -0,0 +1,19 @@
+export const HOSTING_SPECIFIC_DAYS = [
+    { value: 0, name: 'Domingo' },
+    { value: 1, name: 'Segunda' },
+    { value: 2, name: 'Terça' },
+    { value: 3, name: 'Quarta' },
+    { value: 4, name: 'Quinta' },
+    { value: 5, name: 'Sexta' },
+    { value: 6, name: 'Sábado' },
+]
+
+export const AVAILABLE_WEEK_DAYS = [
+    { value: 0, name: 'Domingo' },
+    { value: 1, name: 'Segunda' },
+    { value: 2, name: 'Terça' },
+    { value: 3, name: 'Quarta' },
+    { value: 4, name: 'Quinta' },
+    { value: 5, name: 'Sexta' },
+    { value: 6, name: 'Sábado' },
+]
