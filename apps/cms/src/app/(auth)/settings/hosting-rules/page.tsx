cd1749fb79e37b3dad5705f5d08f90753136bcee
Zayma1 <zaymakinsiona@gmail.com>
2025-03-26 10:12:54 -0300

    changes

--- apps/cms/src/app/(auth)/settings/hosting-rules/page.tsx
+++ apps/cms/src/app/(auth)/settings/hosting-rules/page.tsx
@@ -1,10 +1,50 @@
 'use client'
 
+import { useGetCompanyHostingRules } from '@booksuite/sdk'
+import { useToast } from '@chakra-ui/react'
+import { Formik } from 'formik'
+
+import { useCurrentCompanyId } from '@/common/contexts/user'
 import { PageHeader } from '@/components/organisms/PageHeader'
 
 import { HostingRulesForm } from './components/HostingRulesForm'
+import {
+    createHostingRulesInitialValues,
+    HostingRulesData,
+} from './utils/config'
 
 export default function HostingRules() {
+    const companyId = useCurrentCompanyId()
+
+    const { data: companyHostingRulesData, isLoading } =
+        useGetCompanyHostingRules({
+            companyId: companyId,
+        })
+
+    // const { mutateAsync: updateCompanyHostingRules } = upsertCompanyHostingRules({companyId: companyId}, {})
+
+    const toast = useToast()
+
+    async function handleSubmit(formData: HostingRulesData) {
+        console.log(formData)
+        // try {
+        //     await updateCompany({
+        //         id: companyId,
+        //         data: formData,
+        //     })
+        //     toast({
+        //         title: 'Dados gerais modificados com sucesso ',
+        //         status: 'success',
+        //     })
+        // } catch (error) {
+        //     toast({
+        //         title: 'Erro ao editar dados gerais',
+        //         description: getErrorMessage(error),
+        //         status: 'error',
+        //     })
+        // }
+    }
+
     return (
         <div className="hosting_rules">
             <PageHeader.Root>
@@ -15,7 +55,16 @@ export default function HostingRules() {
                 <PageHeader.Title>Regras de Hospedagem</PageHeader.Title>
             </PageHeader.Root>
 
-            <HostingRulesForm />
+            {!isLoading && (
+                <Formik<HostingRulesData>
+                    initialValues={createHostingRulesInitialValues(
+                        companyHostingRulesData,
+                    )}
+                    onSubmit={handleSubmit}
+                >
+                    <HostingRulesForm />
+                </Formik>
+            )}
         </div>
     )
 }
