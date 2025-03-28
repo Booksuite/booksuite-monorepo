f982c087edd75c990e093bca55a92d44343a276f
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-03-28 08:46:47 -0300

    Cancellation Policy (#48)
    * Created Config.ts
    
    * Created Default Cancellation Rules
    
    * Created Custom Penalty Rates
    
    * Cancellation Policy Description
    
    * Created Cancellation Policy Templates Form
    
    * Changes In Cancellation Policy
    
    * configured formik
    
    * Fixes In Cancellation Policy
    
    * half finished
    
    * Added FormikController To CancellationPolicy
    
    * finished
    
    * fix
    
    ---------
    
    Co-authored-by: Zayma1 <zaymakinsiona@gmail.com>

---
+++ apps/cms/src/app/(auth)/settings/cancellation-policy/utils/constants.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_PENALTY_OPTIONS = [
+    { label: 'Porcentagem de Reserva', value: 'RESERVATION_PERCENTAGE' },
+    {
+        label: 'Porcentagem por Diárias',
+        value: 'FULL_DAILIES_PERCENTAGE',
+    },
+    { label: 'Valor da Primeira Noite', value: 'FIRST_NIGHT_AMOUNT' },
+]
