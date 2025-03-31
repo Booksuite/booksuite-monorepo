c50f72008ce802f27dca25ee3059eec576111bd0
KevinGirelli <kevingirelli.gg@gmail.com>
2025-03-30 21:11:18 -0300

    Created Form

--- apps/cms/src/app/(auth)/settings/business-description/page.tsx
+++ apps/cms/src/app/(auth)/settings/business-description/page.tsx
@@ -1,8 +1,28 @@
 'use client'
 
+import { Formik } from 'formik'
+import { useRouter } from 'next/navigation'
+
+import { FormikController } from '@/components/molecules/FormikController'
 import { PageHeader } from '@/components/organisms/PageHeader'
 
+import { BusinessDescriptionForm } from './components/BusinessDescriptionForm'
+import {
+    BusinessDescriptionFormData,
+    businessDescriptionFormSchema,
+    businessDescriptionInitialValues,
+} from './utils/config'
+
 export default function BusinessDescription() {
+    const { back } = useRouter()
+    function handleSubmit() {
+        try {
+            back()
+        } catch {
+            return null
+        }
+    }
+
     return (
         <div className="business_description">
             <PageHeader.Root>
@@ -10,8 +30,18 @@ export default function BusinessDescription() {
                     Configurações
                 </PageHeader.BackLink>
 
-                <PageHeader.Title>Descricao do Negocio</PageHeader.Title>
+                <PageHeader.Title>Descrição do Negócio</PageHeader.Title>
             </PageHeader.Root>
+
+            <Formik<BusinessDescriptionFormData>
+                initialValues={businessDescriptionInitialValues()}
+                validationSchema={businessDescriptionFormSchema}
+                onSubmit={handleSubmit}
+            >
+                <FormikController onCancel={() => back()}>
+                    <BusinessDescriptionForm />
+                </FormikController>
+            </Formik>
         </div>
     )
 }
