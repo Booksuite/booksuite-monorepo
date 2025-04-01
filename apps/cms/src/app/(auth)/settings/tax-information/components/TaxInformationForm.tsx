7b3b4b5076d56b0c506e29fdaf3e47eb98eea609
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-01 10:57:02 -0300

    Changes In Structure

--- apps/cms/src/app/(auth)/settings/tax-information/components/TaxInformationForm.tsx
+++ apps/cms/src/app/(auth)/settings/tax-information/components/TaxInformationForm.tsx
@@ -12,6 +12,7 @@ import {
 import { useFormikContext } from 'formik'
 import { Info } from 'lucide-react'
 
+import { FormSection } from '@/components/atoms/FormSection'
 import { TaxInformationData } from '../utils/config'
 import { DOC_TYPE } from '../utils/constants'
 
@@ -29,207 +30,202 @@ export const TaxInformationForm = () => {
 
     return (
         <div className="tax_information">
-            <Stack mt={8} spacing={4}>
-                <Typography variant="h6" fontWeight={600} mb={0}>
-                    Dados do Responsável
-                </Typography>
-                <TextField
-                    label="Nome do Responsável"
-                    variant="outlined"
-                    fullWidth
-                    error={!!errors.responsible && touched.responsible}
-                    helperText={errors.responsible}
-                    {...getFieldProps('responsible')}
-                />
-                <TextField
-                    label="E-mail do Responsável"
-                    variant="outlined"
-                    fullWidth
-                    error={
-                        !!errors.responsibleEmail && touched.responsibleEmail
-                    }
-                    helperText={errors.responsibleEmail}
-                    {...getFieldProps('responsibleEmail')}
-                />
-                <TextField
-                    label="Telefone Celular"
-                    variant="outlined"
-                    fullWidth
-                    error={
-                        !!errors.responsiblePhone && touched.responsiblePhone
-                    }
-                    helperText={errors.responsiblePhone}
-                    {...getFieldProps('responsiblePhone')}
-                />
+            <Stack gap={4}>
+                <FormSection title="Dados do Responsável">
+                    <TextField
+                        label="Nome do Responsável"
+                        fullWidth
+                        error={!!errors.responsible && touched.responsible}
+                        helperText={errors.responsible}
+                        {...getFieldProps('responsible')}
+                    />
+                    <TextField
+                        label="E-mail do Responsável"
+                        fullWidth
+                        error={
+                            !!errors.responsibleEmail &&
+                            touched.responsibleEmail
+                        }
+                        helperText={errors.responsibleEmail}
+                        {...getFieldProps('responsibleEmail')}
+                    />
+                    <TextField
+                        label="Telefone Celular"
+                        fullWidth
+                        error={
+                            !!errors.responsiblePhone &&
+                            touched.responsiblePhone
+                        }
+                        helperText={errors.responsiblePhone}
+                        {...getFieldProps('responsiblePhone')}
+                    />
 
-                <Box
-                    bgcolor={'grey.100'}
-                    p={3}
-                    borderRadius={1}
-                    display={'flex'}
-                    alignItems={'center'}
-                    width={'100%'}
-                >
-                    <Box display="flex" alignItems="center" gap={2}>
-                        <Info size={23} color={'#0B1F51'} />
-                        <Typography variant="body1" color={'#0B1F51'}>
-                            <b>Importante:</b> os dados do responsável pela
-                            empresa não serão exibidos para o seu cliente.
-                        </Typography>
+                    <Box
+                        bgcolor={'grey.100'}
+                        p={3}
+                        borderRadius={1}
+                        display={'flex'}
+                        alignItems={'center'}
+                        width={'100%'}
+                        marginTop={4}
+                    >
+                        <Box display="flex" alignItems="center" gap={2}>
+                            <Info size={23} color={'#0B1F51'} />
+                            <Typography variant="body1" color={'#0B1F51'}>
+                                <b>Importante:</b> os dados do responsável pela
+                                empresa não serão exibidos para o seu cliente.
+                            </Typography>
+                        </Box>
                     </Box>
-                </Box>
-            </Stack>
-            <Stack mt={8} spacing={4}>
-                <Typography variant="h6" fontWeight={600} mb={0}>
-                    Informações do Negócio
-                </Typography>
-                <Grid
-                    container
-                    rowSpacing={4}
-                    columnSpacing={{
-                        xs: 1,
-                        sm: 2,
-                        md: 3,
-                    }}
-                >
-                    <Grid size={6}>
-                        <FormControl fullWidth>
-                            <InputLabel>Tipo de documento</InputLabel>
-                            <Select
-                                size="medium"
-                                label="Tipo de documento"
-                                onChange={(selectedOption) =>
-                                    setFieldValue(
-                                        'docType',
-                                        selectedOption.target.value,
-                                    )
-                                }
-                                value={values.docType || ''}
-                            >
-                                {DOC_TYPE.map(({ value }, index) => (
-                                    <MenuItem key={index} value={value}>
-                                        {value}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid size={6}>
-                        <TextField
-                            label={
-                                DOC_TYPE.find(
-                                    (option) => option.value === values.docType,
-                                )?.value || 'CPF'
-                            }
-                            variant="outlined"
-                            fullWidth
-                            error={
-                                !!errors.identification &&
-                                touched.identification
-                            }
-                            helperText={errors.identification}
-                            {...getFieldProps('identification')}
-                            onChange={handleChange('identification')}
-                        />
-                    </Grid>
-                    {isCNPJ && (
-                        <>
-                            <Grid size={4}>
-                                <TextField
-                                    label="Razão Social"
-                                    variant="outlined"
-                                    fullWidth
-                                    error={
-                                        !!errors.companyName &&
-                                        touched.companyName
-                                    }
-                                    helperText={errors.companyName}
-                                    {...getFieldProps('companyName')}
-                                    onChange={(e) =>
+                </FormSection>
+                <FormSection title="Informações do Negócio">
+                    <Grid
+                        container
+                        rowSpacing={4}
+                        columnSpacing={{
+                            xs: 1,
+                            sm: 2,
+                            md: 3,
+                        }}
+                    >
+                        <Grid size={6}>
+                            <FormControl fullWidth>
+                                <InputLabel>Tipo de documento</InputLabel>
+                                <Select
+                                    size="medium"
+                                    label="Tipo de documento"
+                                    onChange={(selectedOption) =>
                                         setFieldValue(
-                                            'companyName',
-                                            e.target.value,
+                                            'docType',
+                                            selectedOption.target.value,
                                         )
                                     }
-                                />
-                            </Grid>
-                            <Grid size={4}>
-                                <TextField
-                                    label="Incrição Estadual (opcional)"
-                                    variant="outlined"
-                                    fullWidth
-                                    error={
-                                        !!errors.stateRegistration &&
-                                        touched.stateRegistration
-                                    }
-                                    helperText={errors.stateRegistration}
-                                    {...getFieldProps('stateRegistration')}
-                                    onChange={handleChange('stateRegistration')}
-                                />
-                            </Grid>
-                            <Grid size={4}>
-                                <TextField
-                                    label="Incrição Municipal (opcional)"
-                                    variant="outlined"
-                                    fullWidth
-                                    error={
-                                        !!errors.municipalRegistration &&
-                                        touched.municipalRegistration
-                                    }
-                                    helperText={errors.municipalRegistration}
-                                    {...getFieldProps('municipalRegistration')}
-                                    onChange={handleChange(
-                                        'municipalRegistration',
-                                    )}
-                                />
-                            </Grid>
-                        </>
-                    )}
-                    <Grid size={8}>
-                        <TextField
-                            label="CEP"
-                            variant="outlined"
-                            fullWidth
-                            error={!!errors.zipcode && touched.zipcode}
-                            helperText={errors.zipcode}
-                            {...getFieldProps('zipcode')}
-                            onChange={handleChange('zipcode')}
-                        />
+                                    value={values.docType || ''}
+                                >
+                                    {DOC_TYPE.map(({ value }, index) => (
+                                        <MenuItem key={index} value={value}>
+                                            {value}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid size={6}>
+                            <TextField
+                                label={
+                                    DOC_TYPE.find(
+                                        (option) =>
+                                            option.value === values.docType,
+                                    )?.value || 'CPF'
+                                }
+                                fullWidth
+                                error={
+                                    !!errors.identification &&
+                                    touched.identification
+                                }
+                                helperText={errors.identification}
+                                {...getFieldProps('identification')}
+                                onChange={handleChange('identification')}
+                            />
+                        </Grid>
+                        {isCNPJ && (
+                            <>
+                                <Grid size={4}>
+                                    <TextField
+                                        label="Razão Social"
+                                        fullWidth
+                                        error={
+                                            !!errors.companyName &&
+                                            touched.companyName
+                                        }
+                                        helperText={errors.companyName}
+                                        {...getFieldProps('companyName')}
+                                        onChange={(e) =>
+                                            setFieldValue(
+                                                'companyName',
+                                                e.target.value,
+                                            )
+                                        }
+                                    />
+                                </Grid>
+                                <Grid size={4}>
+                                    <TextField
+                                        label="Incrição Estadual (opcional)"
+                                        fullWidth
+                                        error={
+                                            !!errors.stateRegistration &&
+                                            touched.stateRegistration
+                                        }
+                                        helperText={errors.stateRegistration}
+                                        {...getFieldProps('stateRegistration')}
+                                        onChange={handleChange(
+                                            'stateRegistration',
+                                        )}
+                                    />
+                                </Grid>
+                                <Grid size={4}>
+                                    <TextField
+                                        label="Incrição Municipal (opcional)"
+                                        fullWidth
+                                        error={
+                                            !!errors.municipalRegistration &&
+                                            touched.municipalRegistration
+                                        }
+                                        helperText={
+                                            errors.municipalRegistration
+                                        }
+                                        {...getFieldProps(
+                                            'municipalRegistration',
+                                        )}
+                                        onChange={handleChange(
+                                            'municipalRegistration',
+                                        )}
+                                    />
+                                </Grid>
+                            </>
+                        )}
+                        <Grid size={8}>
+                            <TextField
+                                label="CEP"
+                                fullWidth
+                                error={!!errors.zipcode && touched.zipcode}
+                                helperText={errors.zipcode}
+                                {...getFieldProps('zipcode')}
+                                onChange={handleChange('zipcode')}
+                            />
+                        </Grid>
+                        <Grid size={4}>
+                            <TextField
+                                label="Estado"
+                                fullWidth
+                                error={!!errors.state && touched.state}
+                                helperText={errors.state}
+                                {...getFieldProps('state')}
+                                onChange={handleChange('state')}
+                            />
+                        </Grid>
+                        <Grid size={6}>
+                            <TextField
+                                label="Cidade"
+                                fullWidth
+                                error={!!errors.city && touched.city}
+                                helperText={errors.city}
+                                {...getFieldProps('city')}
+                                onChange={handleChange('city')}
+                            />
+                        </Grid>
+                        <Grid size={6}>
+                            <TextField
+                                label="País"
+                                fullWidth
+                                error={!!errors.country && touched.country}
+                                helperText={errors.country}
+                                {...getFieldProps('country')}
+                                onChange={handleChange('country')}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid size={4}>
-                        <TextField
-                            label="Estado"
-                            variant="outlined"
-                            fullWidth
-                            error={!!errors.state && touched.state}
-                            helperText={errors.state}
-                            {...getFieldProps('state')}
-                            onChange={handleChange('state')}
-                        />
-                    </Grid>
-                    <Grid size={6}>
-                        <TextField
-                            label="Cidade"
-                            variant="outlined"
-                            fullWidth
-                            error={!!errors.city && touched.city}
-                            helperText={errors.city}
-                            {...getFieldProps('city')}
-                            onChange={handleChange('city')}
-                        />
-                    </Grid>
-                    <Grid size={6}>
-                        <TextField
-                            label="País"
-                            variant="outlined"
-                            fullWidth
-                            error={!!errors.country && touched.country}
-                            helperText={errors.country}
-                            {...getFieldProps('country')}
-                            onChange={handleChange('country')}
-                        />
-                    </Grid>
-                </Grid>
+                </FormSection>
             </Stack>
         </div>
     )
