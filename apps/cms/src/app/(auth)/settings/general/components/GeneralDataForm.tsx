3fcb670d45602e91c21be3713cabcd0c147f73c7
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-04-01 19:51:14 -0300

    Rework In General Data

--- apps/cms/src/app/(auth)/settings/general/components/GeneralDataForm.tsx
+++ apps/cms/src/app/(auth)/settings/general/components/GeneralDataForm.tsx
@@ -1,64 +1,69 @@
 'use client'
 
-import { Flex, Select, Stack } from '@chakra-ui/react'
-import { Form, useFormikContext } from 'formik'
+import { Grid, Select, TextField } from '@mui/material'
+import { useFormikContext } from 'formik'
 import React from 'react'
 
-import InputBox from '@/components/atoms/InputBox'
+import { FormContainer } from '@/components/atoms/FormContainer'
+import { FormSection } from '@/components/atoms/FormSection'
 import { GeneralDataForm } from '../utils/config'
 import { BRAZIL_TIMEZONES, COMPANY_TYPES } from '../utils/constants'
 
 export const GeneralForm: React.FC = () => {
-    const { getFieldProps, touched, errors, setFieldValue } =
-        useFormikContext<GeneralDataForm>()
+    const { getFieldProps, setFieldValue } = useFormikContext<GeneralDataForm>()
 
     return (
-        <Form>
-            <Stack gap={2}>
-                <Flex direction="column">
-                    <InputBox
-                        label="Nome da Propriedade"
-                        error={errors.name}
-                        formControl={{
-                            isInvalid: !!errors.name && touched.name,
-                        }}
-                        {...getFieldProps('name')}
-                    />
-                </Flex>
+        <FormContainer>
+            <FormSection>
+                <TextField
+                    label="Nome da Propriedade"
+                    fullWidth
+                    {...getFieldProps('name')}
+                />
 
-                <Flex justifyContent={'space-between'} w={'100%'} gap={2}>
-                    <Select
-                        value={getFieldProps('type').value}
-                        onChange={(selectedOption) =>
-                            setFieldValue('type', selectedOption.target.value)
-                        }
-                    >
-                        {Object.entries(COMPANY_TYPES).map(([label, value]) => (
-                            <option key={label} value={label}>
-                                {value}
-                            </option>
-                        ))}
-                    </Select>
-
-                    <Select
-                        value={getFieldProps('timezone').value}
-                        onChange={(selectedOption) => {
-                            setFieldValue(
-                                'timezone',
-                                selectedOption.target.value,
-                            )
-                        }}
-                    >
-                        {Object.entries(BRAZIL_TIMEZONES).map(
-                            ([label, value]) => (
-                                <option key={label} value={value}>
-                                    {value}
-                                </option>
-                            ),
-                        )}
-                    </Select>
-                </Flex>
-            </Stack>
-        </Form>
+                <Grid container spacing={2}>
+                    <Grid size={6}>
+                        <Select
+                            value={getFieldProps('type').value}
+                            fullWidth
+                            onChange={(selectedOption) =>
+                                setFieldValue(
+                                    'type',
+                                    selectedOption.target.value,
+                                )
+                            }
+                        >
+                            {Object.entries(COMPANY_TYPES).map(
+                                ([label, value]) => (
+                                    <option key={label} value={label}>
+                                        {value}
+                                    </option>
+                                ),
+                            )}
+                        </Select>
+                    </Grid>
+                    <Grid size={6}>
+                        <Select
+                            value={getFieldProps('timezone').value}
+                            fullWidth
+                            onChange={(selectedOption) => {
+                                setFieldValue(
+                                    'timezone',
+                                    selectedOption.target.value,
+                                )
+                            }}
+                        >
+                            {Object.entries(BRAZIL_TIMEZONES).map(
+                                ([label, value]) => (
+                                    <option key={label} value={value}>
+                                        {value}
+                                    </option>
+                                ),
+                            )}
+                        </Select>
+                    </Grid>
+                </Grid>
+            </FormSection>
+        </FormContainer>
     )
 }
