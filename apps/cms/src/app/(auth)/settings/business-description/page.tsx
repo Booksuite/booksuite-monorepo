71844e68c1670badb3e298a8df60bbd76a3a3c1a
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-04-02 13:11:32 -0300

    Rework Business Description

--- apps/cms/src/app/(auth)/settings/business-description/page.tsx
+++ apps/cms/src/app/(auth)/settings/business-description/page.tsx
@@ -2,7 +2,9 @@
 
 import { Formik } from 'formik'
 import { useRouter } from 'next/navigation'
+import { useSnackbar } from 'notistack'
 
+import { getErrorMessage } from '@/common/utils'
 import { FormikController } from '@/components/molecules/FormikController'
 import { PageHeader } from '@/components/organisms/PageHeader'
 
@@ -15,11 +17,32 @@ import {
 
 export default function BusinessDescription() {
     const { back } = useRouter()
+    const { enqueueSnackbar } = useSnackbar()
+
     function handleSubmit() {
         try {
+            enqueueSnackbar('Descrição do negócio modificado com sucesso', {
+                variant: 'success',
+                anchorOrigin: {
+                    vertical: 'top',
+                    horizontal: 'right',
+                },
+                autoHideDuration: 3000,
+            })
+
             back()
-        } catch {
-            return null
+        } catch (error) {
+            enqueueSnackbar(
+                `Erro ao modificar a descrição do negócio: ${getErrorMessage(error)}`,
+                {
+                    variant: 'error',
+                    anchorOrigin: {
+                        vertical: 'top',
+                        horizontal: 'right',
+                    },
+                    autoHideDuration: 5000,
+                },
+            )
         }
     }
 
