2f6e681687aa794516b34c2ae93336ef5436a8c2
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-03 07:20:09 -0300

    Fixes In Login Form

--- apps/cms/src/app/(public)/login/page.tsx
+++ apps/cms/src/app/(public)/login/page.tsx
@@ -4,22 +4,25 @@ import NextLink from 'next/link'
 import { FormSection } from '@/components/atoms/FormSection'
 
 import DashboardLoginForm from './components/DashboardLoginForm'
+import { Stack } from '@mui/material'
 
 export default async function Login() {
     return (
         <div className="Login">
             <DashboardLoginForm />
 
-            <Link
-                as={NextLink}
-                href="/forgot-my-password"
-                display="block"
-                color="blue.500"
-                fontWeight="regular"
-                fontSize={18}
-            >
-                Esqueceu a senha?
-            </Link>
+            <Stack mt={4} alignItems={'center'}>
+                <Link
+                    as={NextLink}
+                    href="/forgot-my-password"
+                    display="block"
+                    color="blue.500"
+                    fontWeight="regular"
+                    fontSize={18}
+                >
+                    Esqueceu a senha?
+                </Link>
+            </Stack>
         </div>
     )
 }
