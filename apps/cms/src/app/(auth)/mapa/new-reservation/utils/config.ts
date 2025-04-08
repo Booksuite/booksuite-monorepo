f965d29c0c35c8e1810ebbd461684e026d50f283
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-08 16:18:49 -0300

    Created Config.ts

--- apps/cms/src/app/(auth)/mapa/new-reservation/utils/config.ts
+++ apps/cms/src/app/(auth)/mapa/new-reservation/utils/config.ts
@@ -131,7 +131,7 @@ export const createReservationFormInitialValues = (
     endDate: data?.endDate || '',
     totalDays: data?.totalDays ?? null,
     adults: data?.adults ?? null,
-    children: data?.children || [],
+    children: data?.children ?? null,
     notes: data?.notes || '',
     housingUnitId: data?.housingUnit?.id || '',
     services:
