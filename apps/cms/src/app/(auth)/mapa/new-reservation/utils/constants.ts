fb0e2c32c69778e79e9445cdb8348ccaccfb76f3
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-08 14:44:16 -0300

    Created Reservation Summary Component

---
+++ apps/cms/src/app/(auth)/mapa/new-reservation/utils/constants.ts
@@ -0,0 +1,5 @@
+export const RESERVATION_OPTIONS = [
+    { label: 'Orçamento', value: '0' },
+    { label: 'Reservado', value: 'CONFIRMED' },
+    { label: 'Pré-Reserva', value: 'WAITING_LIST' },
+]
