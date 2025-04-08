14d624f25109a7a72b5b8a8b5cab70a4ebd8cb3d
Kévin Girelli <131497909+KevinGirelli@users.noreply.github.com>
2025-04-08 11:01:04 -0300

    Created Marketing Page

--- apps/cms/src/app/(auth)/marketing/page.tsx
+++ apps/cms/src/app/(auth)/marketing/page.tsx
@@ -1,33 +1,97 @@
-import { Stack } from '@chakra-ui/react'
+import {
+    Bell,
+    CircleHelp,
+    FileText,
+    Flag,
+    Image,
+    Laptop,
+    Link2,
+    Map,
+    Newspaper,
+    Star,
+} from 'lucide-react'
+import { Route } from 'next'
 
 import { InternalMenu } from '@/components/organisms/InternalMenu'
 import { PageHeader } from '@/components/organisms/PageHeader'
-import HomeIcon from '@/components/svgs/icons/HomeIcon'
+
+type MenuItem = {
+    icon: React.ReactNode
+    title: string
+    href: Route
+}
+
+const menuItems: MenuItem[] = [
+    {
+        icon: <Flag size={20} />,
+        title: 'Banners',
+        href: `/marketing`,
+    },
+    {
+        icon: <Bell size={20} />,
+        title: 'Notificações',
+        href: `/marketing`,
+    },
+    {
+        icon: <FileText size={20} />,
+        title: 'Páginas de Vendas e Promoções',
+        href: `/marketing`,
+    },
+    {
+        icon: <Image size={20} />,
+        title: 'Galerias',
+        href: `/marketing`,
+    },
+    {
+        icon: <Newspaper size={20} />,
+        title: 'Novidades',
+        href: `/marketing`,
+    },
+    {
+        icon: <Link2 size={20} />,
+        title: 'Link Bio',
+        href: `/marketing`,
+    },
+    {
+        icon: <Star size={20} />,
+        title: 'Avaliações',
+        href: `/marketing`,
+    },
+    {
+        icon: <Map size={20} />,
+        title: 'Roteiro e Guia de Hóspedes',
+        href: `/marketing`,
+    },
+    {
+        icon: <CircleHelp size={20} />,
+        title: 'Roteiro e Guia de Hóspedes',
+        href: `/marketing`,
+    },
+    {
+        icon: <Laptop size={20} />,
+        title: 'Integrações e Códigos',
+        href: `/marketing`,
+    },
+]
 
 export default function Marketing() {
     return (
-        <div className="Marketing">
-            <PageHeader.Root>
-                <PageHeader.BackLink href="/">Início</PageHeader.BackLink>
-
-                <PageHeader.Title>Marketing</PageHeader.Title>
-            </PageHeader.Root>
-
-            <Stack direction={'column'} gap={2}>
-                <InternalMenu.Root>
-                    <InternalMenu.Button href="/marketing/banners">
-                        <HomeIcon />
-                        Banners
-                    </InternalMenu.Button>
-                </InternalMenu.Root>
-
-                <InternalMenu.Root>
-                    <InternalMenu.Button href="/marketing/automacoes">
-                        <HomeIcon />
-                        Automações
-                    </InternalMenu.Button>
-                </InternalMenu.Root>
-            </Stack>
+        <div>
+            <PageHeader
+                title="Marketing"
+                backLButtonLabel="Início"
+                backButtonHref="/"
+            />
+            <InternalMenu.Root>
+                {menuItems.map((item) => (
+                    <InternalMenu.Button
+                        key={item.href}
+                        icon={item.icon}
+                        title={item.title}
+                        href={item.href}
+                    />
+                ))}
+            </InternalMenu.Root>
         </div>
     )
 }
