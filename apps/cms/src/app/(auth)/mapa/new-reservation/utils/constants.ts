74b0957822997041ef5bf831b363b81ef8fbcf32
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-08 14:44:16 -0300

    Created Reservation Summary Component

--- apps/cms/src/app/(auth)/mapa/new-reservation/utils/constants.ts
+++ apps/cms/src/app/(auth)/mapa/new-reservation/utils/constants.ts
@@ -1,29 +1,5 @@
 export const RESERVATION_OPTIONS = [
-    { label: 'Orçamento', value: 'BUDGET' },
+    { label: 'Orçamento', value: '0' },
     { label: 'Reservado', value: 'CONFIRMED' },
     { label: 'Pré-Reserva', value: 'WAITING_LIST' },
-]
-
-export const CHANNEL_OPTIONS = [
-    { label: 'Recepção', value: 'RECEPTION' },
-    { label: 'Telefone', value: 'PHONE' },
-    { label: 'WhatsApp', value: 'WHATSAPP' },
-    { label: 'Instagram', value: 'INSTAGRAM' },
-    { label: 'TikTok', value: 'TIKTOK' },
-    { label: 'Email', value: 'EMAIL' },
-    { label: 'BookSuite', value: 'BOOKSUITE' },
-    { label: 'Outro', value: 'OTHER' },
-]
-
-export const PAYMENT_TIME = [
-    { label: '15 minutos', value: '15m' },
-    { label: '30 minutos', value: '30m' },
-    { label: '1 hora', value: '1h' },
-    { label: '2 horas', value: '2h' },
-    { label: '6 horas', value: '6h' },
-    { label: '12 horas', value: '12h' },
-    { label: '1 dia', value: '24h' },
-    { label: '2 dias', value: '48h' },
-    { label: '3 dias', value: '72h' },
-    { label: '7 dias', value: '168h' },
 ]
