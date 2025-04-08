85362e9b954d07c00d405f8135f50f1761383b5b
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-08 14:44:16 -0300

    Created Reservation Summary Component

--- apps/cms/src/app/(auth)/mapa/new-reservation/page.tsx
+++ apps/cms/src/app/(auth)/mapa/new-reservation/page.tsx
@@ -1,49 +1,11 @@
-'use client'
-
 import { MenuItem, Stack, TextField } from '@mui/material'
-import { Formik } from 'formik'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
 
-import { FormikController } from '@/components/molecules/FormikController'
 import { PageHeader } from '@/components/organisms/PageHeader'
 
-import { BudgetForm } from './components/BudgetForm'
-import { NewReservationForm } from './components/NewReservationForm'
-import { PreReservationForm } from './components/PreReservationForm'
 import ReservationSummary from './components/ReservationSummary'
-import {
-    createReservationFormInitialValues,
-    ReservationFormData,
-    reservationFormSchema,
-} from './utils/config'
 import { RESERVATION_OPTIONS } from './utils/constants'
 
 export default function NewReservation() {
-    const [selectedType, setSelectedType] = useState('')
-    const { back } = useRouter()
-
-    const renderFormByType = () => {
-        switch (selectedType) {
-            case 'BUDGET':
-                return <BudgetForm />
-            case 'CONFIRMED':
-                return <NewReservationForm />
-            case 'WAITING_LIST':
-                return <PreReservationForm />
-            default:
-                return null
-        }
-    }
-
-    const handleSubmit = (values: ReservationFormData) => {
-        try {
-            back()
-        } catch {
-            back()
-        }
-    }
-
     return (
         <>
             <PageHeader
@@ -52,36 +14,18 @@ export default function NewReservation() {
                 backButtonHref="/mapa"
             />
 
-            <Formik<ReservationFormData>
-                initialValues={createReservationFormInitialValues()}
-                validationSchema={reservationFormSchema}
-                onSubmit={handleSubmit}
-            >
-                <FormikController onCancel={() => back()}>
-                    <Stack display="flex" flexDirection="row" gap={7}>
-                        <Stack width="100%">
-                            <TextField
-                                select
-                                label="Tipo de Variação do Preço"
-                                value={selectedType}
-                                onChange={(e) =>
-                                    setSelectedType(e.target.value)
-                                }
-                            >
-                                {RESERVATION_OPTIONS.map(({ label, value }) => (
-                                    <MenuItem key={value} value={value}>
-                                        {label}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
-
-                            {renderFormByType()}
-                        </Stack>
-
-                        <ReservationSummary />
-                    </Stack>
-                </FormikController>
-            </Formik>
+            <Stack display={'flex'} flexDirection={'row'} gap={7}>
+                <Stack width={'100%'}>
+                    <TextField select label="Tipo de Variação do Preço">
+                        {RESERVATION_OPTIONS.map(({ label, value }) => (
+                            <MenuItem key={value} value={value}>
+                                {label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Stack>
+                <ReservationSummary />
+            </Stack>
         </>
     )
 }
