6eb67b3da04faf559e03d93d329acb0a201a08ae
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-09 12:53:44 -0300

    Added Select Logic

--- apps/cms/src/app/(auth)/mapa/new-reservation/utils/constants.ts
+++ apps/cms/src/app/(auth)/mapa/new-reservation/utils/constants.ts
@@ -1,5 +1,5 @@
 export const RESERVATION_OPTIONS = [
-    { label: 'Orçamento', value: '0' },
+    { label: 'Orçamento', value: 'BUDGET' },
     { label: 'Reservado', value: 'CONFIRMED' },
     { label: 'Pré-Reserva', value: 'WAITING_LIST' },
 ]
@@ -13,4 +13,17 @@ export const CHANNEL_OPTIONS = [
     { label: 'Email', value: 'EMAIL' },
     { label: 'BookSuite', value: 'BOOKSUITE' },
     { label: 'Outro', value: 'OTHER' },
+]
+
+export const PAYMENT_TIME = [
+    { label: '15 minutos', value: '15m' },
+    { label: '30 minutos', value: '30m' },
+    { label: '1 hora', value: '1h' },
+    { label: '2 horas', value: '2h' },
+    { label: '6 horas', value: '6h' },
+    { label: '12 horas', value: '12h' },
+    { label: '1 dia', value: '24h' },
+    { label: '2 dias', value: '48h' },
+    { label: '3 dias', value: '72h' },
+    { label: '7 dias', value: '168h' },
 ]
