e0e16a0174c579709ff7dc9b172d6cdf69a4acb2
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-11 11:34:45 -0300

    Privacy Policy Integration

--- apps/cms/src/app/(auth)/settings/privacy-policy/components/PrivacyPolicyForm.tsx
+++ apps/cms/src/app/(auth)/settings/privacy-policy/components/PrivacyPolicyForm.tsx
@@ -1,9 +1,13 @@
 import { TextField } from '@mui/material'
+import { useFormikContext } from 'formik'
 
 import { FormContainer } from '@/components/atoms/FormContainer'
 import { FormSection } from '@/components/atoms/FormSection'
+import { PrivacyPolicyFormData } from '../utils/config'
 
 export const PrivacyPolicyForm = () => {
+    const { getFieldProps, touched, errors, isSubmitting } =
+        useFormikContext<PrivacyPolicyFormData>()
     return (
         <FormContainer>
             <FormSection>
@@ -11,9 +15,17 @@ export const PrivacyPolicyForm = () => {
                     multiline
                     rows={10}
                     label="Outras regras e observações"
-                    /*error={!!errors.}
-                            helperText={errors.}
-                            {...getFieldProps('')}*/
+                    error={
+                        touched.privacyPolicyDescription &&
+                        Boolean(errors.privacyPolicyDescription)
+                    }
+                    helperText={
+                        touched.privacyPolicyDescription &&
+                        errors.privacyPolicyDescription
+                    }
+                    disabled={isSubmitting}
+                    fullWidth
+                    {...getFieldProps('privacyPolicyDescription')}
                 />
             </FormSection>
         </FormContainer>
