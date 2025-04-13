bb08827dbcd6e054e1ecefc9a4a79b8181fe1892
Zaymã Kinsiona <127459308+Zayma1@users.noreply.github.com>
2025-04-13 17:50:57 -0300

    fixed hostingRules (#123)

--- apps/cms/src/app/(auth)/settings/hosting-rules/utils/contants.ts
+++ apps/cms/src/app/(auth)/settings/hosting-rules/utils/contants.ts
@@ -20,26 +20,44 @@ export const AVAILABLE_WEEK_DAYS = [
 
 export const CHECKIN_OPTIONS = [
     { label: '14:00', value: 14 },
+    { label: '14:30', value: 14.5 },
     { label: '15:00', value: 15 },
+    { label: '15:30', value: 15.5 },
     { label: '16:00', value: 16 },
+    { label: '16:30', value: 16.5 },
     { label: '17:00', value: 17 },
+    { label: '17:30', value: 17.5 },
     { label: '18:00', value: 18 },
+    { label: '18:30', value: 18.5 },
     { label: '19:00', value: 19 },
+    { label: '19:30', value: 19.5 },
     { label: '20:00', value: 20 },
+    { label: '20:30', value: 20.5 },
     { label: '21:00', value: 21 },
+    { label: '21:30', value: 21.5 },
     { label: '22:00', value: 22 },
+    { label: '22:30', value: 22.5 },
     { label: '23:00', value: 23 },
+    { label: '23:30', value: 23.5 },
 ]
 
 export const CHECKOUT_OPTIONS = [
     { label: '06:00', value: 6 },
+    { label: '06:30', value: 6.5 },
     { label: '07:00', value: 7 },
+    { label: '07:30', value: 7.5 },
     { label: '08:00', value: 8 },
+    { label: '08:30', value: 8.5 },
     { label: '09:00', value: 9 },
+    { label: '09:30', value: 9.5 },
     { label: '10:00', value: 10 },
+    { label: '10:30', value: 10.5 },
     { label: '11:00', value: 11 },
+    { label: '11:30', value: 11.5 },
     { label: '12:00', value: 12 },
+    { label: '12:30', value: 12.5 },
     { label: '13:00', value: 13 },
+    { label: '13:30', value: 13.5 },
 ]
 
 export const OPENING_WINDOW = [
