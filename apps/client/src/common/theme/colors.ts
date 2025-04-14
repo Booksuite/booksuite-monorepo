9a22768016d60941ded67453974f58f537f31638
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-14 14:51:09 -0300

    Start Structure Folder Of Client

---
+++ apps/client/src/common/theme/colors.ts
@@ -0,0 +1,56 @@
+export const colors = {
+    primary: {
+        color: '',
+    },
+
+    blueGrey: {
+        100: '#F0F4F8',
+        200: '#D9E2EC',
+        300: '#9FB3C8',
+        400: '#627D98',
+        500: '#486581',
+        600: '#334E68',
+        700: '#243B53',
+        800: '#102A43',
+        primary: '#243B53',
+        secondary: '#486581',
+    },
+    coolGrey: {
+        100: '#F5F7FA',
+        200: '#E4E7EB',
+        300: '#CBD2D9',
+        400: '#9AA5B1',
+        500: '#7B8794',
+        600: '#616E7C',
+        700: '#52606D',
+        800: '#3E4C59',
+        primary: '#52606D',
+        secondary: '#7B8794',
+    },
+    grey: {
+        100: '#F7F7F7',
+        200: '#E1E1E1',
+        300: '#CFCFCF',
+        400: '#B1B1B1',
+        500: '#9E9E9E',
+        600: '#7E7E7E',
+        700: '#626262',
+        800: '#515151',
+        primary: '#222222',
+        secondary: '#626262',
+    },
+    warmGrey: {
+        100: '#FAF9F7',
+        200: '#E8E6E1',
+        300: '#D3CEC4',
+        400: '#B8B2A7',
+        500: '#A39E93',
+        600: '#857F72',
+        700: '#625D52',
+        800: '#504A40',
+        primary: '#504A40',
+        secondary: '#857F72',
+    },
+} as const
+
+export type ColorPalette = typeof colors
