e96227fd8afd037da182dafb05ac55b41042feb4
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-14 13:21:39 -0300

    Start Create Client

--- apps/client/components/buttons/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Button as ChakraButton, ButtonProps } from '@chakra-ui/react';
-
-export default function Button(props: ButtonProps) {
-  return <ChakraButton {...props}>{props.children}</ChakraButton>;
-}
--- apps/client/services/acomodacoes/searchAcomodacoes.ts
+++
@@ -1,7 +0,0 @@
-import axiosInstance from "@/lib/axios-instance";
-
-export default async function searchAcomodacoes(payload: any) {
-  const { data } = await axiosInstance.post("/property/search", payload);
-
-  return data ?? [];
-}
