13468068f4739b27b3228d954461093ec31e306b
KevinGirelli <kevingirelli.gg@gmail.com>
2025-04-18 21:06:25 -0300

    Created OthersHome

--- apps/client/src/app/(public)/(home)/page.tsx
+++ apps/client/src/app/(public)/(home)/page.tsx
@@ -3,9 +3,11 @@
 import { About } from './components/About'
 import { BannerFixed } from './components/BannerFixed'
 import { ExtraServices } from './components/ExtraServices'
+import { FooterHome } from './components/FooterHome'
 import { HeaderHome } from './components/HeaderHome'
 import { HousingUnitType } from './components/HousingUnitType'
 import { ReceiveOurPromotions } from './components/ReceiveOurPromotions'
+
 export default function Home() {
     return (
         <div className="relative min-h-screen flex flex-col">
@@ -15,6 +17,7 @@ export default function Home() {
             <ExtraServices />
             <BannerFixed />
             <ReceiveOurPromotions />
+            <FooterHome />
         </div>
     )
 }
